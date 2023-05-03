d8b9b14158ac53d5d939246e26a00141a94736e0
noman17rus <noman171991@gmail.com>
2023-05-03 19:30:13 +0400

    always > 0

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,11 @@
 import java.math.RoundingMode
+import kotlin.math.abs
 import kotlin.math.sqrt
 
 val atmPresure = 100100 //Pa
-val standard = 0.001513 //mg/m3
+val standard = 0.0020513 //mg/m3
 fun main(args: Array<String>) {
-    val s1 = Sample(6.67, 20.1, 0.126, 0.15)
+    val s1 = Sample(4.67, 20.1, 0.126, -0.15)
     val result = getResult(getExpenses(s1), s1)
     println("Норматив = $standard, Массовый выброс = $result")
 }
@@ -29,18 +30,16 @@ private fun getExpenses(sample: Sample): Double {
 }
 
 private fun getResult(expenses: Double, sample: Sample): Double {
-    var check = false
     val filter1 = round(0.09 + (Math.random() / 100))
     var filter2 = round(filter1 + Math.random())
-    while (!false) {
+    while (filter2 > filter1) {
         val concentration =
             round(((filter2 - filter1) * 1000 * atmPresure * (273 + sample.temp)) / (expenses * sample.time * 273 * (atmPresure - sample.presure)))
         val result = concentration * sample.speed * sample.square
         if (result > standard) {
-            filter2 = round(filter2 - 0.0001)
+                filter2 = round(filter2 - Math.random() / 1000)
         } else {
             println("Filter = $filter1, Filter2 = $filter2, Концентрация = $concentration")
-            check = true
             return result
         }
     }
