9f1e8153ddfb9cc1b55c783ad3c31d6ec4a5e139
noman17rus <noman171991@gmail.com>
2023-05-03 13:22:50 +0400

    filters

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,16 +5,18 @@ val atmPresure = 100100
 val standard = 0.021513
 fun main(args: Array<String>) {
     val s1 = Sample(6.22, 27.1, 0.126, 0.15)
-    val result = (getConcentration(getExpenses(s1), s1) * s1.speed * s1.square).toBigDecimal().setScale(6, RoundingMode.UP).toDouble()
+    val result = round(getConcentration(getExpenses(s1), s1) * s1.speed * s1.square)
     println("Массовый выброс = $result")
 }
+
 private fun getExpenses(sample: Sample): Double {
     var check = false
     var expenses: Double = 0.0
     while (!check) {
         for (x in 10 downTo 1) {
             expenses =
-                (2.45 * 0.001 * x * x * sample.speed * (atmPresure - sample.presure) / (273 + sample.temp) * sqrt((273 + sample.temp) / (atmPresure - sample.presure))).toBigDecimal().setScale(1, RoundingMode.UP).toDouble()
+                (2.45 * 0.001 * x * x * sample.speed * (atmPresure - sample.presure) / (273 + sample.temp) * sqrt((273 + sample.temp) / (atmPresure - sample.presure))).toBigDecimal()
+                    .setScale(1, RoundingMode.UP).toDouble()
             if (expenses < 20) {
                 println("Наконечник = $x")
                 check = true
@@ -25,14 +27,14 @@ private fun getExpenses(sample: Sample): Double {
     println("Расход = $expenses")
     return expenses
 }
+
 private fun getConcentration(expenses: Double, sample: Sample): Double {
     var check = false
-    val filter1 = (0.09 + (Math.random() / 100)).toBigDecimal().setScale(4, RoundingMode.UP).toDouble()
-    var filter2 = (filter1 + Math.random()).toBigDecimal().setScale(4, RoundingMode.UP).toDouble()
+    val filter1 = round(0.09 + (Math.random() / 100))
+    var filter2 = round(filter1 + Math.random())
     while (!false) {
         val concentration =
-            (((filter2 - filter1) * 1000 * atmPresure * (273 + sample.temp)) / (expenses * sample.time * 273 * (atmPresure - sample.presure))).toBigDecimal()
-                .setScale(6, RoundingMode.UP).toDouble()
+            round(((filter2 - filter1) * 1000 * atmPresure * (273 + sample.temp)) / (expenses * sample.time * 273 * (atmPresure - sample.presure)))
         if (concentration > standard) {
             filter2 -= 0.0001
         } else {
@@ -44,3 +46,6 @@ private fun getConcentration(expenses: Double, sample: Sample): Double {
     return 0.0
 }
 
+private fun round(value: Double): Double {
+    return value.toBigDecimal().setScale(4, RoundingMode.UP).toDouble()
+}
